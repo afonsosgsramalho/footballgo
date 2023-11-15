8a0f7f7ae1c857dd14c8d7deab2bd441cea2a8ac
afonsosgsramalho <afonsosgsramalho@gmail.com>
2023-11-15 01:29:02 +0000

    videos and colly package added

--- cmd/videos.go
+++ cmd/videos.go
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+=======
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+>>>>>>> 111bb82dbadce9ebe449dd3ff6593fd240fae50e
 package cmd
 
 import (
@@ -9,8 +15,13 @@ import (
 // videosCmd represents the videos command
 var videosCmd = &cobra.Command{
 	Use:   "videos",
+<<<<<<< HEAD
 	Short: "game videos",
 	Long:  `Get videos of specific game for the current month, only if the video is available`,
+=======
+	Short: "team videos",
+	Long:  `Get videos of teams`,
+>>>>>>> 111bb82dbadce9ebe449dd3ff6593fd240fae50e
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("videos called")
 	},
