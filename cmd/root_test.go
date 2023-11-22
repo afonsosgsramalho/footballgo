6225e772ad6dec8a5446363a2a0ce6cc0069eb09
afonsosgsramalho <afonsosgsramalho@gmail.com>
2023-11-22 19:28:52 +0000

    todo tests

---
+++ cmd/root_test.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"testing"
+)
+
+func TestFixturesCommand(t *testing.T) {
+	//TODO
+}
+
+func TestSoresCommand(t *testing.T) {
+	//TODO
+}
+
+func TestStandingsCommand(t *testing.T) {
+	//TODO
+}
+
+func TestVideosCommand(t *testing.T) {
+	//TODO
+}
